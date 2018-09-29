24a45768c5a66a6a6da34648418294f32af26c16
Erik <elin7780@bths.edu>
2018-09-29 15:27:31 -0400

    Express middleware

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const controller = require('./controllers/controller.js')
+const express = require('express');
+const cors = require('cors');
+const parser = require('body-parser');
+const controller = require('./controllers/controller.js');
 
-const db = require('./database/mongoose.js')
-const port = process.env.PORT || 3000
+const db = require('./database/mongoose.js');
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.post('/fountain', controller.fountain.createEntry)
+// Express Middleware
+app.use(parser.json());
+app.use(cors());
+// End of Middleware
+
+app.get('/fountain', controller.getFountain);
+app.post('/fountain', controller.createEntry);
 
 app.listen(port, () => {
-  console.log('Server connected on', port)
-})
+  console.log('Server connected on', port);
+});
