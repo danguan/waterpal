d6f950fd4d2ea5970cd4c2295524ed715e44c189
Erik <elin7780@bths.edu>
2018-09-29 16:40:18 -0400

    Promisify MongoDB, add longitude and latitude to schema

--- server/models/fountain.js
+++ server/models/fountain.js
@@ -4,33 +4,23 @@ const Schema = mongoose.Schema;
 let fountainSchema = new Schema({
   name: { type: String, unique: true },
   borough: String,
+  lng: Number,
+  lat: Number,
   fountains: { type: Number, default: 0 }
 });
 
 const Fountain = mongoose.model('Fountain', fountainSchema);
 
-const getFountain = ({ name }, cb) => {
-  Fountain.find({ name })
-    .exec((err, results) => {
-      if (err) cb(err)
+const getFountain = name => {
+  return Fountain.find({ name })
+};
 
-      cb(null, results)
-    })
-}
-
-const createEntry = ({ site_name, borough, drinking_fountains }, cb) => {
-  Fountain.update(
+const createEntry = ({ site_name, borough, drinking_fountains, lng, lat }) => {
+  return Fountain.updateOne(
     { name: site_name },
-    { name: site_name, borough, $inc: { fountains: drinking_fountains } },
+    { name: site_name, borough, lng, lat, $inc: { fountains: drinking_fountains } },
     { upsert: true }
   )
-    .catch(err => {
-      console.error('Error creating entry:', err.errmsg);
-      cb(err);
-    })
-    .then(response => {
-      cb(null, response);
-    });
 };
 
 module.exports = {
