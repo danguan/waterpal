7d84f7523a5b5cd8c51879ea697118751478ac69
Jack Lim <jacklim.tkl@gmail.com>
2018-09-29 17:45:47 -0400

    added leaflet map

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 var path = require('path')
-require('babel-polyfill')
 
 var SRC_DIR = path.join(__dirname, '/client/src')
 var DIST_DIR = path.join(__dirname, '/client/dist')
 
 module.exports = {
   mode: 'production',
-  entry: ['babel-polyfill', `${SRC_DIR}/index.jsx`],
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
@@ -18,14 +17,30 @@ module.exports = {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         }
       },
       {
         type: 'javascript/auto',
         test: /\.mjs$/,
         include: /node_modules/
-      }
+      },
+      {
+        test:/\.css$/,
+        include: [
+          SRC_DIR,
+          /node_modules/
+        ],
+        loader: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i, 
+        include: [
+          SRC_DIR,
+          /node_modules/
+        ],
+        loader: "file-loader"
+      },
     ]
   }
 }
