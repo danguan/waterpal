6a54ce781029edb84db3f9ee501aaa7f9790d2da
Dan Guan <daniel.guan.bca@gmail.com>
2018-09-30 01:28:20 -0400

    Add react-leaflet-markercluster
    - Cluster close markers
    - Change default marker style

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 // import './styles/css/main.css';
 import L from 'leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import axios from 'axios';
 import 'leaflet/dist/leaflet.css';
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-
-let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow
+let FountainIcon = L.icon({
+  iconUrl: 'http://maps.google.com/mapfiles/ms/micons/orange-dot.png'
 });
-
-L.Marker.prototype.options.icon = DefaultIcon;
 
 export default class App extends React.Component {
   state = {
@@ -34,7 +29,6 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     const position = [40.8, -73.8];
     return (
       <div>
@@ -43,25 +37,34 @@ export default class App extends React.Component {
           style={{ height: '100vh', width: '100vw' }}
           center={position}
           zoom="13"
+          maxZoom="18"
         >
           <TileLayer
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {this.state.markers ? (
-            this.state.markers.map(marker => {
-              return (
-                <Marker position={[marker.lat, marker.lng]}>
-                  <Popup>
-                    {marker.name}
-                    <br />
-                    {`${marker.fountains} Fountain${
-                      marker.fountains === 1 ? '' : 's'
-                    }`}
-                  </Popup>
-                </Marker>
-              );
-            })
+            <MarkerClusterGroup>
+              {this.state.markers.map(marker => {
+                return (
+                  <Marker
+                    key={marker.name}
+                    position={[marker.lat, marker.lng]}
+                    icon={FountainIcon}
+                  >
+                    <Popup>
+                      <a href={marker.url}>{marker.name}</a>
+                      <br />
+                      {marker.address}
+                      <br />
+                      {`${marker.fountains} Fountain${
+                        marker.fountains === 1 ? '' : 's'
+                      }`}
+                    </Popup>
+                  </Marker>
+                );
+              })}
+            </MarkerClusterGroup>
           ) : (
             <div />
           )}
