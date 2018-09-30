c04500c9fb4a1d30a0e4e365d5f78d3e945ab1a0
Dan Guan <daniel.guan.bca@gmail.com>
2018-09-29 21:45:24 -0400

    Add markers based on db call
    - Location, long/lat

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,42 +1,72 @@
-import React from 'react' 
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import React from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 // import './styles/css/main.css';
-import L from 'leaflet'
+import L from 'leaflet';
+import axios from 'axios';
 import 'leaflet/dist/leaflet.css';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
+  iconUrl: icon,
+  shadowUrl: iconShadow
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
-export default class App extends React.Component{
-  state = {}
 
-  render(){
-    const position = [40.8,-73.8];
-    return(
-      <div>WaterPal
-        <Map style={{height:"100vh", width:"100vw"}} center={position} zoom="13">
+export default class App extends React.Component {
+  state = {
+    markers: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('/fountain')
+      .then(({ data }) => {
+        this.setState({
+          markers: data
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    console.log(this.state);
+    const position = [40.8, -73.8];
+    return (
+      <div>
+        WaterPal
+        <Map
+          style={{ height: '100vh', width: '100vw' }}
+          center={position}
+          zoom="13"
+        >
           <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br/> Easily customizable.
-            </Popup>
-          </Marker>
-          <Marker position={[40.9,-72.6]}>
-            <Popup>
-              A pretty CSS3 popup. <br/> Easily customizable.
-            </Popup>
-          </Marker>
+          {this.state.markers ? (
+            this.state.markers.map(marker => {
+              return (
+                <Marker position={[marker.lat, marker.lng]}>
+                  <Popup>
+                    {marker.name}
+                    <br />
+                    {`${marker.fountains} Fountain${
+                      marker.fountains === 1 ? '' : 's'
+                    }`}
+                  </Popup>
+                </Marker>
+              );
+            })
+          ) : (
+            <div />
+          )}
         </Map>
       </div>
-    )
+    );
   }
 }
